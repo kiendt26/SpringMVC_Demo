b64c5629c93512b85ce386b298500773af09340e
Trung Kien <pttrungkien03@gmail.com>
2024-07-29 11:14:42 +0700

    update product controller

--- src/main/java/com/spring/controller/ProductController.java
+++ src/main/java/com/spring/controller/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -33,7 +34,13 @@ public class ProductController {
         ModelAndView m = new ModelAndView("product");
         m.addObject("productList",productService.getAll());
         return m;
+    }
 
+    @GetMapping("/product/{id}")
+    public ModelAndView productDetails(@PathVariable Integer id) {
+        ModelAndView m = new ModelAndView("product-details");
+        m.addObject("product", productService.getById(id));
+        return m;
     }
 
 
